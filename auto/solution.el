725edf1dbbd613d0f342a21734ca15e2094ec2be
Daniel Filan <df@danielfilan.com>
2016-08-27 00:07:16 -0700

    my attempt at extra credit

--- auto/solution.el
+++ auto/solution.el
@@ -15,11 +15,19 @@
     "amsthm"
     "mathtools"
     "pdfpages")
+   (TeX-add-symbols
+    '("ts" 1))
    (LaTeX-add-labels
     "sec:main-problem"
     "sec:tl-dr"
     "sec:proof"
     "eq:2"
     "eq:3"
-    "eq:4")))
+    "eq:4"
+    "sec:extra-credit"
+    "eq:5"
+    "eq:6"
+    "eq:7"
+    "eq:8"
+    "eq:9")))
 
